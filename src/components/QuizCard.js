109dc60cb8f02d322bc00a5574a30ac1ca44ce3d
JorPan <jbpanasewicz@gmail.com>
2021-05-21 16:51:21 -0600

    quiz mode

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,11 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { GiRapidshareArrow } from "react-icons/gi";
 import "./Card.css";
 
-export default function QuizCard({ question, answer }) {
+export default function QuizCard({
+  selectedDeck,
+  questionNumber,
+  cardSide,
+  setCardSide,
+}) {
+  const [cardContent, setCardContent] = useState(selectedDeck.content);
+
+  useEffect(() => {
+    setCardContent(selectedDeck.content[questionNumber]);
+  }, [cardContent, questionNumber, selectedDeck.content]);
+
+  const flipCard = () => {
+    cardSide === "front" ? setCardSide("back") : setCardSide("front");
+  };
+
   return (
     <div className="quiz-card">
-      <p>{question}</p>
-      <p>{answer}</p>
+      <div className="quiz-card-top">
+        <GiRapidshareArrow className="flip-card-button" onClick={flipCard} />
+      </div>
+      <div className="quiz-card-content">
+        {cardSide === "front" ? (
+          <p>{cardContent.front}</p>
+        ) : (
+          <p>{cardContent.back}</p>
+        )}
+      </div>
+      <div className="quiz-card-bottom"></div>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,61 @@
-import React from "react";
+import { useState } from "react";
 import QuizCard from "../components/QuizCard";
+import { BsCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 import "./HomePage.css";
 
 export default function HomePage({ quizMode, selectedDeck }) {
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [cardSide, setCardSide] = useState("front");
+
+  const incrementQuestionNumber = () => {
+    if (questionNumber < selectedDeck.content.length - 1) {
+      setQuestionNumber(questionNumber + 1);
+    } else {
+      setQuestionNumber(0);
+    }
+    setCardSide("front");
+  };
+
+  const decrementQuestionNumber = () => {
+    if (questionNumber === 0) {
+      setQuestionNumber(selectedDeck.content.length - 1);
+    } else {
+      setQuestionNumber(questionNumber - 1);
+    }
+    setCardSide("front");
+  };
+
   return (
     <div className="home-page">
       <div className="sidebar-block"></div>
       {quizMode === false ? (
-        <h1 className="home-page-title">Home Page</h1>
+        <div>
+          <h1 className="home-page-title">Home Page</h1>
+          <p>Select a deck to get started!</p>
+        </div>
       ) : (
         <div>
           <h1 className="home-page-title">Practice Time!</h1>
           <h2 className="selected-deck-title">{selectedDeck.data.name}</h2>
-          {selectedDeck.content.map((question) => (
-            <QuizCard question={question.front} answer={question.back} />
-          ))}
+          <h3 className="card-number">{`${questionNumber + 1}/${
+            selectedDeck.content.length
+          }`}</h3>
+          <div className="quiz-section">
+            <BsCaretLeftFill
+              className="change-question-button"
+              onClick={decrementQuestionNumber}
+            />
+            <QuizCard
+              selectedDeck={selectedDeck}
+              questionNumber={questionNumber}
+              cardSide={cardSide}
+              setCardSide={setCardSide}
+            />
+            <BsFillCaretRightFill
+              className="change-question-button"
+              onClick={incrementQuestionNumber}
+            />
+          </div>
         </div>
       )}
     </div>
